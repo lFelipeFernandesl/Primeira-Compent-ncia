2a7008af7a7446b5b12eac58f88d8d6f3cd24129
Seu Nome <seu-email@example.com>
2024-09-08 22:30:59 -0300

    Primeiro commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Primeira Competencia',
+  webDir: 'www'
+};
+
+export default config;
